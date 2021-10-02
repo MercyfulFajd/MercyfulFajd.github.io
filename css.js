9eeeb1938e2b9793038134c6ca9175f18705913c
MercyfulFajd <banfaikaroly@live.com>
2021-10-02 16:52:59 +0200

    Initial setup

---
+++ css.js
@@ -0,0 +1,8 @@
+
+$("body").addClass("w3-dark-gray");
+
+$("#message").addClass("w3-panel w3-red");
+
+$(".content-row").addClass("w3-row-padding w3-teal");
+
+$(".item").addClass("w3-panel s4 w3-col");
---
+++ main.js
@@ -0,0 +1,1 @@
+$("#message").html("<b>Under Development</b>");
